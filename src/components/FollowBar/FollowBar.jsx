572705e4c2da8f9a6ecd45476411a459df8fc7f5
sadath <sadathshariff65@gmail.com>
2022-05-15 19:26:28 +0530

    feat: Followbar

---
+++ src/components/FollowBar/FollowBar.jsx
@@ -0,0 +1,57 @@
+import styles from "./FollowBar.module.css";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+export const FollowBar = () => {
+  const people = [
+    {
+      id: 1,
+      name: "Sadath",
+    },
+    {
+      id: 2,
+      name: "Nisha",
+    },
+    {
+      id: 3,
+      name: "Aish",
+    },
+    {
+      id: 4,
+      name: "Jenny",
+    },
+    {
+      id: 5,
+      name: "Jyo",
+    },
+    {
+      id: 6,
+      name: "Chanchal",
+    },
+    {
+      id: 7,
+      name: "Mazz",
+    },
+    {
+      id: 8,
+      name: "Yash",
+    },
+  ];
+  return (
+    <>
+      <div className={`${styles.followbar_container}`}>
+        <h3>Suggestions</h3>
+        <ul className="list">
+          {people?.map(({ id, name }) => (
+            <li key={id} className={`${styles.user_info}`}>
+              <Avatar alt={name} src="/static/images/avatar/2.jpg" />
+              <p>{name}</p>
+              <Button variant="outlined" color="secondary">
+                Follow
+              </Button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
