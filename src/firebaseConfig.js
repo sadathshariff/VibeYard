4705d6fdec42e0bd0c53a45ab2f9669dfa6c3952
sadath <sadathshariff65@gmail.com>
2022-05-28 20:19:21 +0530

    Firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCGI47hzOFkm2c5f2EzNM-7J1sx-8fqhs4",
+  authDomain: "vibeyard-1876b.firebaseapp.com",
+  projectId: "vibeyard-1876b",
+  storageBucket: "vibeyard-1876b.appspot.com",
+  messagingSenderId: "237733283385",
+  appId: "1:237733283385:web:697962f274ce55c8652fe8",
+};
+
+export default firebaseConfig;
