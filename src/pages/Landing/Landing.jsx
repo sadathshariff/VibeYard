140872fba214aa6053cde985ddc13c9fed231495
sadath <sadathshariff65@gmail.com>
2022-06-02 16:00:43 +0530

    removed Particles package

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -2,32 +2,14 @@ import styles from "./Landing.module.css";
 import video from "assets/video.mp4";
 import { Link } from "react-router-dom";
 import { Button, Box } from "@mui/material";
-import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
-import { particlesConfig } from "particle-config";
-export const Landing = () => {
-  const particlesInit = async (main) => {
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(main);
-  };
 
-  //Commented out will be working on this later on
-  // const particlesLoaded = (container) => {
-  //   console.log(container);
-  // };
+export const Landing = () => {
   return (
     <>
       <h1 align="center">Welcome to</h1>
 
       <div className={`${styles.home_container}`}>
-        {/* <Particles
-          id="tsparticles"
-          init={particlesInit}
-          // loaded={particlesLoaded}
-          options={particlesConfig}
-        ></Particles> */}
         <section className={`${styles.showcase} flex-center`}>
           {/* <video className={`${styles.video} `} autoPlay loop muted>
             <source src={video} type="video/mp4" />
