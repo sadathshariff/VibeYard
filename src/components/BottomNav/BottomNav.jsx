5597bd9428083ec65b5565ab48eabb17cab26b8a
sadath <sadathshariff65@gmail.com>
2022-06-02 15:59:56 +0530

    Added logout

--- src/components/BottomNav/BottomNav.jsx
+++ src/components/BottomNav/BottomNav.jsx
@@ -3,9 +3,21 @@ import { BiHomeSmile } from "react-icons/bi";
 import { MdTravelExplore } from "react-icons/md";
 import { BsBookmark } from "react-icons/bs";
 import { FiUser, FiLogOut } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "firebase.js";
+import { openToast } from "redux/features/toastSlice";
+import { useDispatch } from "react-redux";
 
 export const BottomNav = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const logout = () => {
+    signOut(auth);
+    localStorage.removeItem("userToken");
+    navigate("/");
+    dispatch(openToast({ message: "Logout successful", type: "success" }));
+  };
   return (
     <>
       <ul className={`${styles.bottomNav_container} list flex justify-sb`}>
@@ -31,7 +43,7 @@ export const BottomNav = () => {
         </li>
         <li>
           <Link to="/">
-            <FiLogOut size={25} />
+            <FiLogOut size={25} onClick={() => logout()} />
           </Link>
         </li>
       </ul>
--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -1,54 +1,71 @@
 import styles from "./SideNav.module.css";
-import { Link } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import { BiHomeSmile } from "react-icons/bi";
 import { MdTravelExplore } from "react-icons/md";
 import { BsBookmark } from "react-icons/bs";
 import { FiUser, FiLogOut } from "react-icons/fi";
+import { signOut } from "firebase/auth";
+import { auth } from "firebase.js";
+import { openToast } from "redux/features/toastSlice";
+import { useDispatch } from "react-redux";
 
 export const SideNav = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const logout = () => {
+    signOut(auth);
+    localStorage.removeItem("userToken");
+    navigate("/");
+    dispatch(openToast({ message: "Logout successful", type: "success" }));
+  };
+  const getActiveStyle = ({ isActive }) =>
+    isActive
+      ? {
+          backgroundColor: "var(--primary-color)",
+          
+        }
+      : {};
   return (
     <div className={`${styles.sidenav_container} `}>
       <ul className="list">
         <li>
-          <Link to="/feed">
+          <NavLink to="/feed" style={getActiveStyle}>
             <div className={`${styles.sidenav_item}`}>
               <BiHomeSmile size={27} />
               <p className={`${styles.nav_item}`}>Home</p>
             </div>
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/explore">
+          <NavLink to="/explore" style={getActiveStyle}>
             <div className={`${styles.sidenav_item}`}>
               <MdTravelExplore size={27} />
               <p className={`${styles.nav_item}`}>Explore</p>
             </div>
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/bookmark">
+          <NavLink to="/bookmark" style={getActiveStyle}>
             <div className={`${styles.sidenav_item}`}>
               <BsBookmark size={23} />
               <p className={`${styles.nav_item}`}>Saved</p>
             </div>
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/profile">
+          <NavLink to="/profile" style={getActiveStyle}>
             <div className={`${styles.sidenav_item}`}>
               <FiUser size={25} />
               <p className={`${styles.nav_item}`}>Profile</p>
             </div>
-          </Link>
+          </NavLink>
         </li>
         <li>
           <div className={`${styles.sidenav_item}`}>
-            <Link to="/">
-              <div className={`${styles.sidenav_item}`}>
-                <FiLogOut size={25} />
-                <p className={`${styles.nav_item}`}>Logout</p>
-              </div>
-            </Link>
+            <div className={`${styles.sidenav_item}`} onClick={() => logout()}>
+              <FiLogOut size={25} />
+              <p className={`${styles.nav_item}`}>Logout</p>
+            </div>
           </div>
         </li>
       </ul>
