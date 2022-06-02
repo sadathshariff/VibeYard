df212ccf1b3febde83a57e86f182f60558c07787
sadath <sadathshariff65@gmail.com>
2022-06-02 22:57:16 +0530

    fix: user Bug

--- src/components/ProfileAuth/ProfileAuth.jsx
+++ src/components/ProfileAuth/ProfileAuth.jsx
@@ -2,8 +2,6 @@ import { useSelector } from "react-redux";
 import { Navigate, useParams, Outlet } from "react-router-dom";
 export const ProfileAuth = () => {
   const { userName } = useParams();
-  const { allusers } = useSelector((store) => store.allUsers);
-  const { token } = useSelector((store) => store.user);
-  const userId = allusers.find((p) => p?.data?.userName === userName)?.id;
-  return token !== userId ? <Outlet /> : <Navigate to="/profile" />;
+  const { user } = useSelector((store) => store.user);
+  return userName !== user?.userName ? <Outlet /> : <Navigate to="/profile" />;
 };
