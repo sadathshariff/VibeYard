99c10dbcf69f9c846c981aed74f1e589a19eb98b
sadath <sadathshariff65@gmail.com>
2022-06-02 15:53:06 +0530

    Feat : "Not Found Page"

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,3 +1,27 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import NotFoundImage from "assets/NotFoundImage.svg";
+import { useNavigate } from "react-router-dom";
 export const NotFound = () => {
-  return <>Not Found</>;
+  const navigate = useNavigate();
+  return (
+    <Box
+      sx={{
+        width: "50%",
+        height: "50%",
+
+        margin: "0 auto",
+      }}
+    >
+      <img src={NotFoundImage} alt="Page Not Found" />
+
+      <Button
+        variant="contained"
+        color="success"
+        onClick={() => navigate("/feed", { replace: true })}
+      >
+        Back to Feed
+      </Button>
+    </Box>
+  );
 };
