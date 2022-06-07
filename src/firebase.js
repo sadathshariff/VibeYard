686aefdc755fd01252d4c498e0cdceea069e61de
sadath <sadathshariff65@gmail.com>
2022-06-07 23:15:50 +0530

    Firebase Storage

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 import firebaseConfig from "firebaseConfig.js";
 
 //  acts as entire point between firebase and React app
@@ -10,4 +11,6 @@ const auth = getAuth(firebaseApp);
 
 const db = getFirestore(firebaseApp);
 
-export { auth, db };
+const storage = getStorage(firebaseApp);
+
+export { auth, db, storage };
